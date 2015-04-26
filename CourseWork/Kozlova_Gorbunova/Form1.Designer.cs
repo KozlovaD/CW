d7512ae9b310bf8d9c93fb53392ab68ef6ab3358
Darya Gorbunova <daria73@gmail.com>
2015-04-26 21:09:50 +0300

    Изменения в базе данных

--- CourseWork/Kozlova_Gorbunova/Form1.Designer.cs
+++ CourseWork/Kozlova_Gorbunova/Form1.Designer.cs
@@ -28,6 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.Главная = new System.Windows.Forms.TabPage();
             this.Отчет = new System.Windows.Forms.TabPage();
@@ -167,17 +173,66 @@
             // 
             // dataGridView1
             // 
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle5;
             this.dataGridView1.Location = new System.Drawing.Point(15, 26);
             this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
             this.dataGridView1.Size = new System.Drawing.Size(640, 137);
             this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // dataGridView2
             // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView2.Location = new System.Drawing.Point(16, 182);
             this.dataGridView2.Name = "dataGridView2";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridView2.Size = new System.Drawing.Size(639, 142);
             this.dataGridView2.TabIndex = 3;
             // 
--- CourseWork/Kozlova_Gorbunova/Form1.cs
+++ CourseWork/Kozlova_Gorbunova/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 
 using System.Data.SQLite;
+//using System.Data.SqlServerCe;
 
 namespace Koz_Gor_kurs
 {
@@ -16,6 +17,73 @@ namespace Koz_Gor_kurs
         public Form1()
         {
             InitializeComponent();
+            //MessageBox.Show("Hello");
+             FillData();
+        }
+
+        void FillData()
+        {
+            //SQLiteConnection.CreateFile("base.db");
+
+            SQLiteConnection cnn = new SQLiteConnection("Data Source = base.db");
+            DataTable dt = new DataTable();
+            SQLiteCommand cmd;
+            cnn.Open();
+
+            //cmd = new SQLiteCommand("CREATE TABLE kosmetika(id int PRIMARY KEY, type TEXT, name TEXT, stoimost DOUBLE, count INTEGER, end_date DATE)", cnn);
+            //cmd = new SQLiteCommand("UPDATE kosmetika SET count = 3 WHERE id = 1", cnn);
+            //cmd = new SQLiteCommand( "CREATE TABLE client(id int PRIMARY KEY, fio TEXT, sym DOUBLE)", cnn);
+            //cmd = new SQLiteCommand("CREATE TABLE harakteristika(id int PRIMARY KEY, name TEXT)", cnn);
+            //cmd = new SQLiteCommand("CREATE TABLE harakteristika_kosmetika(id_kosmetika int, id_harakteristika int)", cnn);
+            //cmd = new SQLiteCommand("CREATE TABLE harakteristika_client(id_client int, id_harakteristika int)", cnn);
+            //cmd = new SQLiteCommand("CREATE TABLE prodaja(id int PRIMARY KEY, data DATE, sym DOUBLE, skidka DOUBLE)", cnn);
+            //cmd = new SQLiteCommand("CREATE TABLE list_prodaja(id_prodaja int, id_client int, id_kosmetika int, count int, sym DOUBLE)", cnn);
+            //cmd.ExecuteNonQuery();
+
+            cmd = new SQLiteCommand("SELECT * FROM kosmetika ORDER BY end_date", cnn);
+            dt.Load(cmd.ExecuteReader());
+            dataGridView2.DataSource = dt;
+
+            dt = new DataTable();
+
+            cmd = new SQLiteCommand("SELECT * FROM kosmetika WHERE count < 5 ORDER BY count", cnn);
+            dt.Load(cmd.ExecuteReader());
+            dataGridView1.DataSource = dt;
+
+            cnn.Close();
+
+/*
+            var sql_con = new SQLiteConnection("Data Source = base.db");
+            var txtQuery = "SELECT * FROM kosmetika ORDER BY time_end_kosmetika";
+            //var txtQuery = "INSERT INTO `kosmetika` (`id`, `type`, `name`, `stoimost`, `count`, `end_date`) VALUES (2, 'pomada', 'pomada_2', 140, 10, '2016-04-01');";
+            sql_con.Open();
+
+            var sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+
+            sql_cmd.ExecuteNonQuery();
+            sql_con.Close();
+*/            
+            /*
+            using (SqlCeConnection c = new SqlCeConnection(
+Properties.Settings.Default.DatabaseConnectionString))
+            {
+                c.Open();
+                // 2
+                // Create new DataAdapter
+                using (SqlCeDataAdapter a = new SqlCeDataAdapter(
+                    "SELECT * FROM kosmetika ORDER BY time_end_kosmetika", c))
+                {
+                    // 3
+                    // Use DataAdapter to fill DataTable
+                    DataTable t = new DataTable();
+                    a.Fill(t);
+                    // 4
+                    // Render data onto the screen
+                    dataGridView2.DataSource = t;
+                }
+            }*/
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,9 +91,9 @@ namespace Koz_Gor_kurs
 
         }
 
-        private void tabPage2_Click(object sender, EventArgs e)
+        private void tabControl1_Click  (object sender, EventArgs e)
         {
-
+           
         }
 
         private void Главная_Click(object sender, EventArgs e)
@@ -39,6 +107,11 @@ namespace Koz_Gor_kurs
         }
 
         private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
